8b2a642a1cb5dbe6c3279c0ba959265831890497
Shuo Yang <imsure95@gmail.com>
2013-10-02 16:43:27 -0500

    Parser modified to extract month information

--- src/main/java/cloudera/lab/LogRecordParser.java
+++ src/main/java/cloudera/lab/LogRecordParser.java
@@ -5,17 +5,32 @@ import java.util.regex.*;
 public class LogRecordParser {
 	private String record;
 	private static final String IPRegex = "^\\d+\\.\\d+\\.\\d+\\.\\d+";
+	private static final String MonthRegex = "\\[\\d+/(\\w+)/";
+	
+	private Pattern pattern;
+	private Matcher matcher;
 	
 	public void feedInput(String input) {
 		record = input;
 	}
 	
 	public String extractIPAddress() {
-		Pattern pattern = Pattern.compile(IPRegex);
-		Matcher matcher = pattern.matcher(record);
+		pattern = Pattern.compile(IPRegex);
+		matcher = pattern.matcher(record);
 		
 		if (matcher.find()) {
 			return matcher.group();
+		} else {
+			return null;
+		}
+	}
+	
+	public String extractMonth() {
+		pattern = Pattern.compile(MonthRegex);
+		matcher = pattern.matcher(record);
+		
+		if (matcher.find()) {
+			return matcher.group(1);
 		} else {
 			return null;
 		}
@@ -26,5 +41,7 @@ public class LogRecordParser {
 		parser.feedInput("10.223.157.186 - - [15/Jul/2009:14:58:59 -0700] \"GET / HTTP/1.1\" 403 202");
 		String ip = parser.extractIPAddress();
 		System.out.println(ip);
+		String month = parser.extractMonth();
+		System.out.println(month);
 	}
 }
